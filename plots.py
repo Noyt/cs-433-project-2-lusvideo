88b5ed68a88f6ee1b9ddd998dcc5b1e12942e19e
Noyt <arnaudemmanuel.robert@epfl.ch>
2020-12-17 02:37:28 +0100

    Rerunning

--- plots.py
+++ plots.py
@@ -1,17 +1,15 @@
 import matplotlib.pyplot as plt
 
 
-
-
 def plot(arr1, arr2, label1, label2, title, save_path, kind, figsize=(10,5)):
     """
-    TODO
+    Save the plot of the two given arrays
     """
     plt.figure(figsize=(10, 5))
     plt.plot(arr1, "o", linestyle='solid', label=label1)
     plt.plot(arr2, "o", linestyle='solid', label=label2)
     if kind.lower() == 'accuracy':
-        plt.ylim(top = 1, bottom = 0.4)
+        plt.ylim(top = 1.1, bottom = 0.4)
     plt.title(title)
     plt.legend()
     plt.grid()
